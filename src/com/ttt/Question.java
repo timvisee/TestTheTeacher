6790997d3ac7477722c29fc4b097313ed1114ec8
timvisee <timvisee@gmail.com>
2015-10-13 15:50:19 +0200

    Added JavaDocs to Question class, added method to check if answer is correct

--- src/com/ttt/Question.java
+++ src/com/ttt/Question.java
@@ -6,29 +6,74 @@ import java.util.List;
 
 public class Question {
 
+    /** The question. */
     private String question;
+    /** List of answers. */
     private List<String> answers = new ArrayList<>();
+    /** The index of the correct answer. */
     private int correctAnswer;
 
+    /**
+     * Constructor.
+     *
+     * @param question Quetion.
+     * @param answers Four answers in a list.
+     * @param correctAnswer The index of the correct answer.
+     */
     public Question(String question, List<String> answers, int correctAnswer) {
         this.question = question;
         this.answers = answers;
         this.correctAnswer = correctAnswer;
     }
 
+    /**
+     * Constructor.
+     *
+     * @param question Question.
+     * @param answers Four answers in an array.
+     * @param correctAnswer The index of the correct answer.
+     */
     public Question(String question, String[] answers, int correctAnswer) {
         this(question, Arrays.asList(answers), correctAnswer);
     }
 
+    /**
+     * Get the question.
+     *
+     * @return The question.
+     */
     public String getQuestion() {
         return this.question;
     }
 
+    /**
+     * Get the answer by it's index.
+     *
+     * @param i Answer index.
+     *
+     * @return The answer.
+     */
     public String getAnswer(int i) {
         return this.answers.get(i);
     }
 
+    /**
+     * Get the index of the answer that is correct.
+     *
+     * @return Correct answer index.
+     */
     public int getCorrectAnswerIndex() {
         return this.correctAnswer;
+    }
+
+    /**
+     * Check whether the specified answer index is correct.
+     *
+     * @param i The answer index to check.
+     *
+     * @return True if the answer is correct, false if not.
+     */
+    public boolean isCorrectAnswerIndex(int i) {
+        return getCorrectAnswerIndex() == i;
     }
 }
