bccef9b7cf2b89433bf88421b9236e69d449d2a7
timvisee <timvisee@gmail.com>
2015-10-19 19:33:44 +0200

    Created QuizManager class

---
+++ src/com/ttt/quiz/QuizManager.java
@@ -0,0 +1,17 @@
+package com.ttt.quiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizManager {
+
+    /**
+     * List of quizzes.
+     */
+    private List<Quiz> quizzes = new ArrayList<>();
+
+    /**
+     * Constructor.
+     */
+    public QuizManager() { }
+}
