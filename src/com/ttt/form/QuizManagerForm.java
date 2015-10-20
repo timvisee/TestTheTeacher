1c69c87b075bcbfa232141038dd22fe8479409dd
timvisee <timvisee@gmail.com>
2015-10-20 02:00:55 +0200

    Added base of quiz manager form

---
+++ src/com/ttt/form/QuizManagerForm.java
@@ -0,0 +1,138 @@
+/**
+ * TestTheTeacher.
+ * Test the teacher to see whether he or she is really good enough at basing programming.
+ * A project for a school of applied sciences.
+ *
+ * @author MingYan Li, Tim Visee
+ * @copyright Copyright (c) MingYan Li & Tim Visee 2015. All rights reserved.
+ * @license GNU GPL v3.0
+ */
+
+package com.ttt.form;
+
+import com.ttt.App;
+import com.ttt.question.Question;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class QuizManagerForm extends JFrame {
+
+    /** Frame title. */
+    private static final String FORM_TITLE = App.APP_NAME + " - Quiz Manager";
+
+    /** App instance. */
+    private App app;
+
+    /** The quiz manager label. */
+    private JLabel mainLabel = new JLabel("<html>Quiz manager.");
+
+    /**
+     * Constructor.
+     *
+     * @param app App instance.
+     * @param show True to show the frame once it has been initialized.
+     */
+    public QuizManagerForm(App app, boolean show) {
+        // Construct the form
+        super(FORM_TITLE);
+
+        // Store the app instance
+        this.app = app;
+
+        // Create the form UI
+        createUIComponents();
+
+        // Close application when closing form
+        // TODO: Should we keep this?
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Show the form
+        this.setVisible(show);
+    }
+
+    /**
+     * Create all UI components for the frame.
+     */
+    private void createUIComponents() {
+        // Construct a grid bag constraints object to specify the placement of all components
+        GridBagConstraints c = new GridBagConstraints();
+
+        // Set the frame layout
+        this.setLayout(new GridBagLayout());
+
+        // Set the frame size
+        this.setSize(400, 215);
+
+        // Make the frame non-resizable
+        this.setResizable(true);
+
+        // Set the window location to the system's default
+        this.setLocationByPlatform(true);
+        this.setLocationRelativeTo(null);
+
+        // Create the main panel, to put the question and answers in
+        JPanel pnlMain = new JPanel();
+        pnlMain.setLayout(new GridBagLayout());
+
+        // Create the questions panel and set the layout
+        JPanel pnlQuestion = new JPanel();
+        pnlQuestion.setLayout(new GridBagLayout());
+
+        // Configure the placement of the questions label, and add it to the questions panel
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(0, 0, 10, 0);
+        pnlQuestion.add(mainLabel, c);
+
+        // Configure the placement of the questions panel and add it to the main panel
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(0, 0, 0, 0);
+        pnlMain.add(pnlQuestion, c);
+
+        // Create the quiz manager list and add it to the main panel
+        JPanel quizList = createQuizList();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.insets = new Insets(0, 5, 0, 5);
+        c.weightx = 1.0;
+        pnlMain.add(quizList, c);
+
+        // Configure the main panel placement and add it to the frame
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(10, 10, 10, 10);
+        this.add(pnlMain, c);
+    }
+
+    // TODO: Handle quizzes, instead of questions here!
+    /**
+     * Create a list to manage the quizzes in.
+     *
+     * @return Panel with the list.
+     */
+    public JPanel createQuizList() {
+        // Create a panel for the list and set the layout
+        JPanel listPnl = new JPanel();
+        listPnl.setLayout(new BorderLayout());
+
+        // Create a list model for the quizzes
+        // TODO: Add quizzes here instead of questions!
+        DefaultListModel<Question> questionModel = new DefaultListModel<>();
+        questionModel.addElement(this.app.getQuestionById(0));
+        questionModel.addElement(this.app.getQuestionById(1));
+        questionModel.addElement(this.app.getQuestionById(2));
+
+        // Create the list
+        JList<Question> quizList = new JList<>(questionModel);
+
+        // Add the list to the panel
+        listPnl.add(quizList);
+
+        // Return the panel
+        return listPnl;
+    }
+}
