a919096c87f86cb6564932538ddb3a493e1c1743
Tim Visée <timvisee@gmail.com>
2015-10-20 02:03:05 +0200

    Added proper header comments to file

--- src/com/ttt/quiz/Quiz.java
+++ src/com/ttt/quiz/Quiz.java
@@ -1,3 +1,13 @@
+/**
+ * TestTheTeacher.
+ * Test the teacher to see whether he or she is really good enough at basing programming.
+ * A project for a school of applied sciences.
+ *
+ * @author MingYan Li, Tim Visee
+ * @copyright Copyright (c) MingYan Li & Tim Visee 2015. All rights reserved.
+ * @license GNU GPL v3.0
+ */
+
 package com.ttt.quiz;
 
 import com.ttt.question.Question;
