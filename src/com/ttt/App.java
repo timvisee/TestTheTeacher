121106258693cb54c7bead64d7478869477bacfb
mingli <mngyn.li@gmail.com>
2015-10-26 13:59:39 +0100

    added question

--- src/com/ttt/App.java
+++ src/com/ttt/App.java
@@ -263,23 +263,18 @@ public class App {
 
                 },
                 0));
-//        this.mainQuiz.addQuestion(new Question(
-//                "<b>10.  </b><br />" +
-//                        "<br />" +
-//                        StringUtils.indentHtmlSpaces(4) + "int []a = {1,2,3,4,5,6};<br />" +
-//                        StringUtils.indentHtmlSpaces(4) + "int i = a.length - 1;<br />" + "<br />" +
-//                        StringUtils.indentHtmlSpaces(4) + "while(i>=0){<br />" +
-//                        StringUtils.indentHtmlSpaces(8) + "System.out.print(a[i]);#sthash.NhjUgVjX.dpuf<br />" +
-//                        StringUtils.indentHtmlSpaces(8) + "i--;<br />" +
-//                        StringUtils.indentHtmlSpaces(4) + "}<br />",
-//                new String[]{
-//                        "654321",
-//                        "65432",
-//                        "123456",
-//                        "12345",
-//
-//                },
-//                0));
+        this.mainQuiz.addQuestion(new Question(
+                "<b>10. Wat is de uitkomst van <i>a</i>? </b><br />" +
+                        "<br />" +
+                        StringUtils.indentHtmlSpaces(4) + "char a = 'A' + 34;<br />",
+                new String[]{
+                        "A",
+                        "a",
+                        "A34",
+                        "c",
+
+                },
+                3));
     }
 
     /**
